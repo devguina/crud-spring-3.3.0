70bd66c2227a0fa75514cb0a4d160b2f26b9db9b
devguina <guisouzaskill@gmail.com>
2024-08-29 17:42:10 -0300

    adjustents in JPA

--- backend/src/main/resources/db/migration/V1__Populate_initial.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO tb_category (id, name) VALUES
-     (1, 'Drinks'), (2, 'Food');
-
-
-INSERT INTO tb_user (id, email,"name","password") VALUES
-	 (1, 'nina@gmail.com','Nina Brown','$2a$10$yLOYL.VHX9RwdrcDqd73fuhaoWtD.efwDCCDnd5PvT9ItJKbv5PbW'),
-	 (2, 'leia@gmail.com','Leia Red','$2a$10$yLOYL.VHX9RwdrcDqd73fuhaoWtD.efwDCCDnd5PvT9ItJKbv5PbW'),
-	 (3, 'teste@teste.com','teste','$2a$10$.NbkgmS9FyvBTSz3TaRUnejCk3vgJKMCo72aM7mrjp.ilVgg/wGvm');
